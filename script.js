f8cc7f7490c916477cb286df5a32562fa2de2c0a
S. I. Khan <79093080+Swar-i-K@users.noreply.github.com>
2026-08-12 00:27:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function timeDate {
-    const dt = newdate();
+    const dt = newDate();
     document.getElementById('dt3').innerText = dt.toLocaleString();     // Only Date & Time
 }
 timeDate();
