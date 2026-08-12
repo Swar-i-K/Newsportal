ea1a3bbe3b95ff595f9d049e7914c763d60b1cc7
S. I. Khan <79093080+Swar-i-K@users.noreply.github.com>
2026-08-12 20:05:20 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+// Date & Time
+
 function timeDate() {
     const dt = new Date();
     document.getElementById('dt1').innerText = dt.toLocaleString();     // Only Date & Time
 }
 timeDate();
 setInterval(timeDate, 1000);
+
+
+// email alert
+function MsgAlert(){
+            alert('You are about to send an email to info@khulnapaperweb.com');
+        }
+
+
+// call confirmation
+function MsgConfirm(){
+            confirm('Do you want to make a call?');
+        }
